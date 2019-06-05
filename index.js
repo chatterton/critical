888bc48fa2035d9da14a1bf689c5313c4509adf8
Jack Chatterton <jack.chatterton@twistle.com>
2019-06-05 00:18:24 -0700

    initial command line parsing

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 
 const files = require('./src/files')
+const cmd = require('commander')
 
-console.log('Hello, world!')
-console.log('We are located in: ' + files.getCurrentDirectory())
+console.log('Critical Northwest events parser in ' + files.getCurrentDirectory())
+
+cmd
+  .version('0.0.1', '-v, --version')
+  .option('-i, --init <filename>', 'initialize internal database from saved website json')
+  .parse(process.argv)
+
+// Automatically display help if no arguments
+if (!process.argv.slice(2).length) {
+  cmd.outputHelp()
+}
