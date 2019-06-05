626715ffa3f476246e77ebccae4ef2668d62e71b
Jack Chatterton <jack.chatterton@twistle.com>
2019-06-04 23:58:05 -0700

    add critical executable + hello world

---
+++ index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const files = require('./src/files')
+
+console.log('Hello, world!')
+console.log('We are located in: ' + files.getCurrentDirectory())
---
+++ src/files.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+module.exports = {
+  getCurrentDirectoryBase: () => {
+    return path.basename(process.cwd())
+  },
+
+  getCurrentDirectory: () => {
+    const cwd = process.cwd()
+    return path.dirname(cwd) + path.sep + path.basename(cwd)
+  },
+
+  directoryExists: (filePath) => {
+    try {
+      return fs.statSync(filePath).isDirectory()
+    } catch (err) {
+      return false
+    }
+  }
+}
